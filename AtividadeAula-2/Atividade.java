1d2b8285fb6498c6c1434796537d8c7d6619bbbc
Driinho <pedro.nascimento.henrique2003@gmail.com>
2022-06-21 21:59:04 -0300

    diminuindo repetição de código

--- AtividadeAula-2/Atividade.java
+++ AtividadeAula-2/Atividade.java
@@ -8,6 +8,7 @@ public class Atividade {
 
         Random sort = new Random();
         long tempoBinario, tempoSequencial;
+        int tam = 1;
         int chave;
         String tabela = "============================================";
         tabela = tabela + "\n============ Tempo de Execucoes ============";
@@ -15,75 +16,39 @@ public class Atividade {
         tabela = tabela + "\n=   Vetor   =   Sequencial   =   Binario   =";
         tabela = tabela + "\n============================================";
 
-        int[] vetor = new int[10];
+        int[] vetor;
 
-        for (int i = 0; i < vetor.length; i++) {
-            vetor[i] = sort.nextInt(20);
-        }
-        chave = sort.nextInt(20);
+        do {
 
-        tempoSequencial = testeTempoSeq(vetor, chave);
-        tempoBinario = testeTempoBin(vetor, chave);
+            tam = tam * 10;
 
-        // GERANDO TABELA
-        tabela = tabela + "\n=   10      ";
-        tabela = gerarTabela(tabela, tempoBinario, tempoSequencial);
+            vetor = new int[tam];
 
-        vetor = new int[100];
+            for (int i = 0; i < vetor.length; i++) {
+                vetor[i] = sort.nextInt(20);
+            }
 
-        for (int i = 0; i < vetor.length; i++) {
-            vetor[i] = sort.nextInt(200);
-        }
-        chave = sort.nextInt(200);
+            chave = sort.nextInt(20);
 
-        tempoSequencial = testeTempoSeq(vetor, chave);
-        tempoBinario = testeTempoBin(vetor, chave);
+            tempoSequencial = testeTempoSeq(vetor, chave);
+            tempoBinario = testeTempoBin(vetor, chave);
 
-        // GERANDO TABELA
-        tabela = tabela + "\n=   100     ";
-        tabela = gerarTabela(tabela, tempoBinario, tempoSequencial);
+            // GERANDO TABELA
+            if(tam == 10) {
+                tabela = tabela + "\n=   10      ";
+            } else if(tam == 100) {
+                tabela = tabela + "\n=   100     ";
+            } else if(tam == 1000) {
+                tabela = tabela + "\n=   1000    ";
+            } else if(tam == 10000) {
+                tabela = tabela + "\n=   10000   ";
+            } else {
+                tabela = tabela + "\n=   100000  ";
+            }
+            tabela = gerarTabela(tabela, tempoBinario, tempoSequencial);
 
-        vetor = new int[1000];
+        } while(tam != 100000);
 
-        for (int i = 0; i < vetor.length; i++) {
-            vetor[i] = sort.nextInt(2000);
-        }
-        chave = sort.nextInt(2000);
-
-        tempoSequencial = testeTempoSeq(vetor, chave);
-        tempoBinario = testeTempoBin(vetor, chave);
-
-        // GERANDO TABELA
-        tabela = tabela + "\n=   1000    ";
-        tabela = gerarTabela(tabela, tempoBinario, tempoSequencial);
-
-        vetor = new int[10000];
-
-        for (int i = 0; i < vetor.length; i++) {
-            vetor[i] = sort.nextInt(20000);
-        }
-        chave = sort.nextInt(20000);
-
-        tempoSequencial = testeTempoSeq(vetor, chave);
-        tempoBinario = testeTempoBin(vetor, chave);
-
-        // GERANDO TABELA
-        tabela = tabela + "\n=   10000   ";
-        tabela = gerarTabela(tabela, tempoBinario, tempoSequencial);
-
-        vetor = new int[100000];
-
-        for (int i = 0; i < vetor.length; i++) {
-            vetor[i] = sort.nextInt(200000);
-        }
-        chave = sort.nextInt(200000);
-
-        tempoSequencial = testeTempoSeq(vetor, chave);
-        tempoBinario = testeTempoBin(vetor, chave);
-
-        // GERANDO TABELA
-        tabela = tabela + "\n=   100000  ";
-        tabela = gerarTabela(tabela, tempoBinario, tempoSequencial);
         tabela = tabela + "\n============================================";
 
         limpaTerminal();
